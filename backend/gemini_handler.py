c340f968e35cfcdef6978bf28f9e9eada3cbd04f
AryanV-Coder <aryanv.india1@gmail.com>
2025-06-12 15:57:36 +0530

    first commit

---
+++ backend/gemini_handler.py
@@ -0,0 +1,56 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-2.0-flash-lite")
+
+with open("moodgenie_training_data.json",'r') as file:
+    history = json.load(file)
+
+full_conversation = [
+    {
+    "role": "user",
+    "parts": [
+      {
+        "text": "You are MoodGenie, an emotionally intelligent assistant who identifies the user's mood based on conversation and gives tips, quotes, or jokes accordingly. Speak in the language the user uses. You are playful but insightful, empathetic yet humorous. You can roast the user lightly if the tone allows."
+      }
+    ]
+  }
+]
+
+chat = model.start_chat(history=history)
+
+def get_ai_response(user_prompt: str):
+    global history, full_conversation
+
+    response = chat.send_message(user_prompt,stream=True)
+    response.resolve() # Ensure the response is fully generated
+
+    history.append({"role" : "user","parts":[{"text":user_prompt}]})
+    history.append({"role" : "model","parts":[{"text":response.text}]})
+
+    full_conversation.append({"role" : "user","parts":[{"text":user_prompt}]})
+    full_conversation.append({"role" : "model","parts":[{"text":response.text}]})
+
+    return response.text
+
+def summary_of_conversation():
+    global full_conversation
+
+    prompt = '''
+                Based on the conversation you had with the user up till now -
+                1. Identify the overall **mood or emotional journey** of the user in 1–2 sentences (e.g., started low, became cheerful; or stayed frustrated throughout).
+                2. Suggest a short and relevant **motivational quote or a funny joke**, depending on the user’s final mood.
+                3. Write the summary in a way that it can be **saved in the user’s history**. Keep the tone friendly and personal.
+                4. Use the same **language** the user spoke in (can be Hindi, English, or Hinglish).
+             '''
+
+    chat = model.start_chat(history=full_conversation)
+    response = chat.send_message(prompt,stream=True)
+    response.resolve() # Ensure the response is fully generated
+
+    return response.text
